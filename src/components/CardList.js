7502649e121b0117dcb53dc9b55c1f6441cce697
AAKASH PANDITA <aakash2711522@gmail.com>
2020-07-11 01:11:29 +0530

    update
    update

---
+++ src/components/CardList.js
@@ -0,0 +1,19 @@
+import Card from './Card';
+import React from 'react';
+
+
+const CardList = ({robots}) => {
+	return(
+		robots.map((user, i) => {
+				return (
+    			<Card
+    			id={robots[i].id} 
+    			name={robots[i].name}
+    			email={robots[i].email}
+    	 		/>
+    		);
+    		})
+		);
+}
+
+export default CardList;
